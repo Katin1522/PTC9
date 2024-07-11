d950685fbc756d6e14eb4c75b199e63580565611
katin1522 <20220026@ricaldone.edu.sv>
2024-07-11 00:29:51 -0600

    Trabajo

--- app/src/main/java/katherine/ceron/ptc9/Recuperar_Contra.kt
+++ app/src/main/java/katherine/ceron/ptc9/Recuperar_Contra.kt
@@ -15,7 +15,7 @@ class Recuperar_Contra : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recuperar_contra)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtHistorialExp)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/katherine/ceron/ptc9/notificaciones.kt
+++ app/src/main/java/katherine/ceron/ptc9/notificaciones.kt
@@ -11,7 +11,7 @@ class notificaciones : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_notificaciones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtHistorialExp)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
