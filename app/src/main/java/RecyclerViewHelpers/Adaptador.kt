d2669f48bce240892b35e660f5583ae97436e7f5
katin1522 <20220026@ricaldone.edu.sv>
2024-07-13 21:42:55 -0600

    a

---
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -0,0 +1,23 @@
+package RecyclerViewHelpers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import katherine.ceron.ptc9.R
+import modelo.Recetas
+
+class Adaptador(var DatosR: List<Recetas>): RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_cad,parent,false)
+        return ViewHolder(vista)
+    }
+
+    override fun getItemCount() = DatosR.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = DatosR [position]
+        holder.txtNombreCard.text = item.Paciente
+
+    }
+}
