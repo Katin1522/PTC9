fb623d63d549f9c7a5df3744ef2a8c1242a1caf9
Agreda <robertoagreda.inuyasha25@gmail.com>
2024-07-15 00:35:06 -0600

    dgbtg

--- app/src/main/java/RecyclerViewHelpers/viewHolderMedi.kt
+++ app/src/main/java/RecyclerViewHelpers/viewHolderMedi.kt
@@ -3,10 +3,8 @@ package RecyclerViewHelpers
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-<<<<<<< HEAD
 class viewHolderMedi (View: View): RecyclerView.ViewHolder(View){
 
 
 }
-=======
->>>>>>> 5302487ee96ce609f9c35edb4b71a13e03a07641
+
